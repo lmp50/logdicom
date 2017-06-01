d8fa4012f20f304fe9a140992ced22bee6b1337e
lmp50 <lmp@mail.ru>
2017-06-01 13:52:28 +0300

    В меню добавлен пункт выход

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,52 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+//*****************************************************************************
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMenu * mnFile = new QMenu("File"); // создаём меню Файл
+
+    QAction *msgOpenFile = new QAction("Open file",mnFile);
+    connect(msgOpenFile, SIGNAL(triggered()), this, SLOT(slotOpenFile()));
+    mnFile->addAction(msgOpenFile);
+
+    QAction *msgOpenFolder = new QAction("Open folder",mnFile);
+    connect(msgOpenFolder, SIGNAL(triggered()), this, SLOT(slotOpenFolder()));
+    mnFile->addAction(msgOpenFolder);
+
+    QAction *msgExit = new QAction("Exit",mnFile);
+    connect(msgExit, SIGNAL(triggered()), this, SLOT(slotExit()));
+    mnFile->addAction(msgExit);
+    ui->menuBar->addMenu(mnFile); // Добавляем пункты меню в menuBar, т.е. те, которые будут отображаться в гл. окне
+
+    QMenu * mnHelp = new QMenu("Help"); // Меню помощь
+    ui->menuBar->addMenu(mnHelp);
 }
 
+//*****************************************************************************
+void MainWindow::slotExit()
+{
+    close();
+}
+
+//*****************************************************************************
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+//*****************************************************************************
+void MainWindow::slotOpenFile()
+{
+
+}
+
+//*****************************************************************************
+void MainWindow::slotOpenFolder()
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,10 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private slots:
+    void slotOpenFile();
+    void slotOpenFolder();
+    void slotExit();
 
 private:
     Ui::MainWindow *ui;
