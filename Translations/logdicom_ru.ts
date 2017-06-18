fa145d9e31722b9c4dbd045adbc63cf1ff4d0d4d
lmp50h <lmp@mail.ru>
2017-06-18 21:15:21 +0300

    Русское меню

---
+++ Translations/logdicom_ru.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="13"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="10"/>
+        <source>File</source>
+        <translation type="unfinished">Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="12"/>
+        <source>Open file</source>
+        <translation type="unfinished">Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="16"/>
+        <source>Open folder</source>
+        <translation type="unfinished">Открыть папку</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="20"/>
+        <source>Exit</source>
+        <translation type="unfinished">Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <source>Help</source>
+        <translation type="unfinished">Помощь</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="27"/>
+        <source>Contents</source>
+        <translation type="unfinished">Содержание</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="31"/>
+        <source>About logdicom</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
