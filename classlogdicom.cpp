11623e0700a26529311bce244e37df017f2097fc
lmp50h <lmp@mail.ru>
2017-12-24 12:27:47 +0300

    Создан класс всего приложения

---
+++ classlogdicom.cpp
@@ -0,0 +1,6 @@
+#include "classlogdicom.h"
+
+classLogdicom::classLogdicom(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ classlogdicom.h
@@ -0,0 +1,17 @@
+#ifndef CLASSLOGDICOM_H
+#define CLASSLOGDICOM_H
+
+#include <QObject>
+
+class classLogdicom : public QObject
+{
+    Q_OBJECT
+public:
+    explicit classLogdicom(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // CLASSLOGDICOM_H
